cd52f9266c77defd40a6a8f808b7bbcdea05d595
DevHN <devhn0@gmail.com>
2026-05-04 18:00:19 +0900

    [승재] BaseProjectile.h 생성

---
+++ OneTwoShoot/Source/OneTwoShoot/Public/BaseProjectile.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+// DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnExplosionHit, FVector, HitLocation, float, ExplosionRadius);
+
+UCLASS()
+class ONETWOSHOOT_API ABaseProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ABaseProjectile();
+	
+	virtual void Tick(float DeltaTime) override;
+	
+	void FireInDirection(const FVector& ShootDirection, float ShootPower, FVector StageWindForce);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	float ProjectileDamage = 50.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	float RadiusPerDamage = 2.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	float BaseExplosionRadius = 100.0f;
+	
+	// UPROPERTY(BlueprintAssignable, Category = "Combat|Events")
+	// FOnExplosionHit OnExplosionHit;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* CollisionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* ProjectileMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	UProjectileMovementComponent* ProjectileMovement;
+	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	
+private:
+	FVector ConstantWindAcceleration;
+};
