4b257d290f2595fa932a15074f5e1c1521b5ad06
DevHN <devhn0@gmail.com>
2026-05-07 15:15:57 +0900

    [승재] BaseItem 클래스 생성

---
+++ OneTwoShoot/Source/OneTwoShoot/Private/BaseItem.cpp
@@ -0,0 +1,27 @@
+#include "BaseItem.h"
+#include "BaseTank.h"
+
+UBaseItem::UBaseItem()
+{
+	ItemName = TEXT("기본 아이템");
+	ItemDescription = FText::FromString(TEXT("기본 아이템 설명입니다."));
+	ItemType = EItemType::None;
+	ItemIcon = nullptr;
+}
+
+void UBaseItem::UseItem(ABaseTank* User)
+{
+	if (User)
+	{
+		UE_LOG(LogTemp, Log, TEXT("%s 가 %s 아이템을 사용했습니다."), *User->GetName(), *ItemName.ToString());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("아이템 사용 실패. 사용자가 유효하지 않습니다."));
+	}
+}
+
+bool UBaseItem::ConsumOnUse() const
+{
+	return true;
+}
