2df88e49f0f5d2abb6e946b18ebe1bb7c1deef82
Opalian <opalian94@gmail.com>
2026-05-13 19:58:49 +0900

    [정훈] 게임 인스턴스, 게임 세이브 연동해서 추가. TurnGameMode와는 아직 연동 안됨. 세이브 관련 구조체 헤더 GameSaveTypes 추가. MenuGameMode 더미 생성

--- OneTwoShoot/Source/OneTwoShoot/Private/Game/OneTwoShootGameInstance.cpp
+++ OneTwoShoot/Source/OneTwoShoot/Private/Game/OneTwoShootGameInstance.cpp
@@ -1,5 +1,101 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "../Public/Game/OneTwoShootGameInstance.h"
+#include "Game/OneTwoShootGameInstance.h"
 
+#include "Game/GameSave.h"
+#include "Kismet/GameplayStatics.h"
+
+UOneTwoShootGameInstance::UOneTwoShootGameInstance()
+{
+	CurrentSave = nullptr;
+	SaveSlotName = TEXT("OneTwoShoot_Save");
+	SaveUserIndex = 0;
+}
+
+void UOneTwoShootGameInstance::Init()
+{
+	Super::Init();
+
+	LoadProgress();
+}
+
+void UOneTwoShootGameInstance::StartNewRun()
+{
+	RunData = FRunData();
+	RunData.bHasActiveRun = true;
+	RunData.CurrentStageIndex = 0;
+
+	SaveProgress();
+}
+
+bool UOneTwoShootGameInstance::LoadProgress()
+{
+	USaveGame* LoadedSave = UGameplayStatics::LoadGameFromSlot(SaveSlotName, SaveUserIndex);
+	CurrentSave = Cast<UGameSave>(LoadedSave);
+
+	if (!CurrentSave)
+	{
+		CreateFreshSave();
+		return false;
+	}
+
+	ApplySaveToRuntimeData();
+	return true;
+}
+
+bool UOneTwoShootGameInstance::SaveProgress()
+{
+	if (!CurrentSave)
+	{
+		CreateFreshSave();
+	}
+
+	ApplyRuntimeDataToSave();
+	return CurrentSave && UGameplayStatics::SaveGameToSlot(CurrentSave, SaveSlotName, SaveUserIndex);
+}
+
+bool UOneTwoShootGameInstance::HasActiveRun() const
+{
+	return RunData.bHasActiveRun;
+}
+
+int32 UOneTwoShootGameInstance::GetCurrentStageIndex() const
+{
+	return RunData.CurrentStageIndex;
+}
+
+void UOneTwoShootGameInstance::SetCurrentStageIndex(int32 NewStageIndex)
+{
+	RunData.CurrentStageIndex = FMath::Max(0, NewStageIndex);
+}
+
+void UOneTwoShootGameInstance::CreateFreshSave()
+{
+	CurrentSave = Cast<UGameSave>(UGameplayStatics::CreateSaveGameObject(UGameSave::StaticClass()));
+	ApplySaveToRuntimeData();
+}
+
+void UOneTwoShootGameInstance::ApplySaveToRuntimeData()
+{
+	if (!CurrentSave)
+	{
+		ProfileData = FPlayerProfileData();
+		RunData = FRunData();
+		return;
+	}
+
+	ProfileData = CurrentSave->ProfileData;
+	RunData = CurrentSave->RunData;
+}
+
+void UOneTwoShootGameInstance::ApplyRuntimeDataToSave()
+{
+	if (!CurrentSave)
+	{
+		return;
+	}
+
+	CurrentSave->ProfileData = ProfileData;
+	CurrentSave->RunData = RunData;
+}
