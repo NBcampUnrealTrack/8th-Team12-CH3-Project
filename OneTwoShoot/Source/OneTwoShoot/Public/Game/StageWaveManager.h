4ef2b52e49ee1c7d42fac775ec9ec44be9ffa809
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-26 15:21:19 +0900

    [민기] 버그 수정. 플레이어 탱크에도 이펙트+사운드 추가.

--- OneTwoShoot/Source/OneTwoShoot/Public/Game/StageWaveManager.h
+++ OneTwoShoot/Source/OneTwoShoot/Public/Game/StageWaveManager.h
@@ -16,25 +16,27 @@ public:
 protected:
     virtual void BeginPlay() override;
 
-	// 에디터에서 할당할 데이터 테이블 변수
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stage Config")
 	UDataTable* StageDataTable;
 
-	// 현재 어떤 스테이지를 불러올지 결정하는 키값 (Row Name)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stage Config")
 	FName TargetStageRowName;
 
-	// 테이블에서 불러온 실제 웨이브 데이터들을 담아둘 내부 배열
 	TArray<FWaveData> ActiveStageWaves;
 
 	int32 CurrentWaveIndex = 0;
 	int32 CurrentWaveEnemyCount = 0;
 
+	FTimerHandle BattleStartTimerHandle;
+
+	bool bIsWaveStartPending = false;
+	bool bIsWaveActive = false;
+
 public:
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stage Config")
-    float SpawnDropHeight = 2000.f; // 하늘에서 떨어질 높이
+    float SpawnDropHeight = 2000.f;
 
-    void StartNextWave(); // 다음 웨이브 시작
-    void OnEnemyDestroyed(); // 적 처치 시 호출될 함수
+    void StartNextWave();
+    void OnEnemyDestroyed();
 };
