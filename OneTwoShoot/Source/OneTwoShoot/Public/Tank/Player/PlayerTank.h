1502ab8970c3f2cf0292fac121f84dfa15b8ebba
DevHN <devhn0@gmail.com>
2026-05-26 15:53:09 +0900

    Revert "[승재] PlayerTank Phase 분리"
    This reverts commit 2c05194940d65dfec2f051468e84bb6cc057c531.

--- OneTwoShoot/Source/OneTwoShoot/Public/Tank/Player/PlayerTank.h
+++ OneTwoShoot/Source/OneTwoShoot/Public/Tank/Player/PlayerTank.h
@@ -35,7 +35,6 @@ protected:
 	class UInputAction* FireAction;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	class UInputAction* AimAction;
-	
 	/// ----- 아이템 사용을 위한 선언
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input|Item")
 	class UInputAction* UseItem1Action;
@@ -57,27 +56,14 @@ protected:
 	class USpringArmComponent* SpringArm;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	class UCameraComponent* Camera;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Turn|Movement")
-	float MaxMoveDistance = 500.0f;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn|Movement")
-	FVector TurnStartLocation;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	class UCameraComponent* FirstPersonCamera;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
-	class UInputAction* ConfirmPhaseAction;
-	
 	// 드론으로 진입 하기 전의 탱크 시야를 저장하는 변수
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	FRotator SavedTankRotation = FRotator::ZeroRotator;
 	
+	
 	/// ----- 드론 매핑
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input|Drone")
 	TSubclassOf<class ADrone> DroneClass;
-	
 	// 현재 월드에 소환된 드론을 기억하고 지우기 위한 포인터 변수
 	UPROPERTY(VisibleAnywhere, Category = "Input|Drone")
 	class ADrone* SpawnedDrone;
@@ -106,7 +92,6 @@ protected:
 	void Input_UseItem2();
 	void Input_UseItem3();
 	void Input_UseItem4();
-	void Input_ConfirmPhase();
 	virtual void BeginPlay() override;
 	virtual void OnTurnStart() override;
 	virtual void OnTurnEnd() override;
